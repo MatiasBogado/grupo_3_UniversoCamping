3f0a51a6b213fa3b4ba88422248de889f14ed2a2
Matias Bogado <matiasbogado22@hotmail.com>
2020-08-25 18:33:00 -0300

    agregando carpeta data

--- site/data/products.js
+++ site/data/products.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const path = require('path')
 
-module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, './dbProducts.json'), 'utf-8'))
+module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, './products.json'), 'utf-8'))
--- site/data/usuarios.js
+++ site/data/usuarios.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 const path = require('path')
 
-module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, './dbUsuarios.json'), 'utf-8'))
+module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, './users.json'), 'utf-8'))
