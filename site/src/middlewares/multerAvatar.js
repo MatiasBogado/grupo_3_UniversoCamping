e35dcb4f7284960006e8bb32e0b98590f235c210
unknown <f.n.melgarejo@gmail.com>
2020-09-21 17:13:22 -0300

    arreglos menores

--- site/src/middlewares/multerAvatar.js
+++ site/src/middlewares/multerAvatar.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 let storage = multer.diskStorage({
     destination:(req,file,callback)=>{
-        callback(null,'../public/img/avatares')
+        callback(null,'./public/img/avatares')
     },
     filename:(req,file,callback)=>{
         callback(null,file.fieldname + Date.now() + path.extname(file.originalname))
