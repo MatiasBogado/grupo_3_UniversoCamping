3939af157f0d6b9f4a75714a038eb6ae11a94092
Ezequielz <zapata.ed1989@gmail.com>
2020-11-19 04:59:47 -0300

    detalles

--- site/src/database/config/config.js
+++ site/src/database/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "universo_campingg",
+    "database": "universo_camping",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
